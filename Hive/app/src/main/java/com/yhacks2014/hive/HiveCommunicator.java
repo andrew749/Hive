daeebb688083f7b411cc336e6730b931167ce74a
Andrew Codispoti <andrewcod749@gmail.com>
2014-11-01 01:10:50 -0400

    Added method to get event info which can actually take an id paramter.

--- Hive/app/src/main/java/com/yhacks2014/hive/HiveCommunicator.java
+++ Hive/app/src/main/java/com/yhacks2014/hive/HiveCommunicator.java
@@ -1,5 +1,7 @@
 package com.yhacks2014.hive;
 
+
+import android.net.Uri;
 import android.util.Log;
 
 import org.apache.http.HttpEntity;
@@ -11,6 +13,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpParams;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -19,21 +22,25 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+
 /**
  * Created by andrew on 01/11/14.
  */
 public class HiveCommunicator {
-    String token="";
+    final static String token="";
     public HiveCommunicator(){
 
     }
 
-    String URL = "http://the/url/here";
+    String URL = "http://hive-events.herokuapp.com/event/info/";
     String result = "";
-    public void callWebService(String q){
+    public void getEventInfo(String id){
         HttpClient httpclient = new DefaultHttpClient();
-        HttpGet request = new HttpGet(URL + q);
-        ResponseHandler<String> handler = new BasicResponseHandler();
+        HttpGet request = new HttpGet(URL);
+        HttpParams params=request.getParams();
+        params.setParameter("id",id);
+request.setParams(params);
+                ResponseHandler<String> handler = new BasicResponseHandler();
         try {
             result = httpclient.execute(request, handler);
         } catch (ClientProtocolException e) {
