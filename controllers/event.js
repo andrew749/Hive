f55d2d515598fe977814d511f36f52a0ce882371
Yu Chen Hou <houyuchen66@gmail.com>
2014-11-02 02:41:28 -0500

    RSVP system

--- controllers/event.js
+++ controllers/event.js
@@ -301,8 +301,7 @@ exports.postConfirm = function(req, res) {
     query_id =  new _ObjectId(decoded.iss);
     User.findOne({ _id: query_id }, function(err, user) {
       req.user = user;
-        });
-  if (!req.user){
+          if (!req.user){
     console.log("can't");
     return res.json({error:"Not logged in"});
   }
@@ -323,6 +322,8 @@ exports.postConfirm = function(req, res) {
       });
     });
   }
+        });
+
 };
 
 /**
